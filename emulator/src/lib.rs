08bb509c06c6220ae2f64550c30ba32632f7402c
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-12-16 13:59:55 +0100

    feat: split emulator and exposed methods, added smiley face

--- emulator/src/lib.rs
+++ emulator/src/lib.rs
@@ -1,9 +1,12 @@
+mod chip8;
 use wasm_bindgen::prelude::*;
 
+extern crate console_error_panic_hook;
+
 #[wasm_bindgen]
-pub fn greet() {
-    web_sys::window()
-        .unwrap()
-        .alert_with_message("WebAssembly")
-        .unwrap();
+pub fn start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    let emulator = chip8::Emulator::new();
+    emulator.smiley_face();
 }
