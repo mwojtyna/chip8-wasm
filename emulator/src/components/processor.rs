777468eea830e4d257e2f27bcb3f1dd34b888dca
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-12-18 01:38:07 +0100

    feat: add fetch stage, merge processor and memory into one file, add tests

--- emulator/src/components/processor.rs
+++ emulator/src/components/processor.rs
@@ -1,6 +1,28 @@
+use array_init::array_init;
 use web_sys::*;
 
-#[derive(Default, Debug)]
+const FONT_SET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+const FONT_BEGIN_INDEX: usize = 0x50;
+const ROM_BEGIN_INDEX: usize = 0x200;
+
+#[derive(Debug)]
 pub struct Processor {
     /** Program counter - points to the current instruction in the memory */
     pc: u16,
@@ -20,17 +42,68 @@ pub struct Processor {
     /** 16 8-bit registers, named V0 to VF. */
     /** VF is also used as a flag register; many instructions will set it to either 1 or 0 based on some rule, for example using it as a carry flag */
     v: [u8; 16],
+
+    memory: [u8; 4096],
 }
 
 impl Processor {
     pub fn init() -> Processor {
         Processor {
-            pc: 0x200,
-            ..Default::default()
+            memory: array_init(|_| 0),
+            pc: ROM_BEGIN_INDEX as u16,
+            i: 0,
+            stack: Vec::new(),
+            delay_timer: 0,
+            sound_timer: 0,
+            v: array_init(|_| 0),
         }
     }
 
-    pub fn cycle(&self) {
-        console::log_1(&"Cycle".into());
+    pub fn cycle(&mut self) {
+        // Fetch data
+        let instruction = Self::fetch(self.memory, self.pc);
+        self.pc += 2;
+
+        console::log_1(&format!("Instruction: {:#06X}", instruction).into());
+    }
+    fn fetch(memory: [u8; 4096], pc: u16) -> u16 {
+        let first_nibble = memory[pc as usize] as u16;
+        let second_nibble = memory[pc as usize + 1] as u16;
+
+        (first_nibble) << 0x8 | second_nibble
+    }
+
+    pub fn load_fonts(&mut self) {
+        for (i, font) in FONT_SET.iter().enumerate() {
+            self.memory[i + FONT_BEGIN_INDEX] = *font;
+        }
+    }
+    pub fn load_rom(&mut self, rom: Vec<u8>) {
+        for (i, value) in rom.iter().enumerate() {
+            self.memory[ROM_BEGIN_INDEX + i] = *value;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::components::processor::Processor;
+    use array_init::array_init;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    #[wasm_bindgen_test]
+    fn test_fetch() {
+        // Setup
+        let mut memory: [u8; 4096] = array_init(|_| 0);
+        memory[0] = 0xab;
+        memory[1] = 0xcd;
+        let pc: u16 = 0x0;
+
+        // Run
+        let result = Processor::fetch(memory, pc);
+
+        // Assert
+        let expected = 0xabcd;
+        assert_eq!(result, expected, "{:X} =/= {:X}", result, expected);
     }
 }
--- emulator/src/lib.rs
+++ emulator/src/lib.rs
@@ -1,10 +1,9 @@
 mod components {
-    pub mod memory;
     pub mod processor;
     pub mod screen;
 }
-use components::*;
 
+use components::*;
 use fluvio_wasm_timer::Delay;
 use std::time::Duration;
 use wasm_bindgen::prelude::*;
@@ -15,19 +14,17 @@ extern crate console_error_panic_hook;
 
 struct Emulator {
     processor: processor::Processor,
-    memory: memory::Memory,
     screen: screen::Screen,
 }
 impl Emulator {
     pub fn init(width: u32, height: u32) -> Emulator {
         Emulator {
             processor: processor::Processor::init(),
-            memory: memory::Memory::init(),
             screen: screen::Screen::init(width, height),
         }
     }
 
-    pub async fn start(&self) {
+    pub async fn start(&mut self) {
         loop {
             self.processor.cycle();
             Delay::new(Duration::from_secs_f32(1.0))
@@ -41,12 +38,15 @@ impl Emulator {
 pub fn start(width: u32, height: u32) {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    let emulator = Emulator::init(width, height);
+    let mut emulator = Emulator::init(width, height);
+    emulator.screen.test_display();
+    emulator.processor.load_fonts();
+    emulator
+        .processor
+        .load_rom(include_bytes!("roms/ibm-logo.ch8").to_vec());
 
-    console::log_1(&format!("{:?}", emulator.processor).into());
-    console::log_1(&format!("{:?}", emulator.memory).into());
+    console::log_1(&format!("{:#x?}", emulator.processor).into());
     console::log_1(&format!("{:?}", emulator.screen).into());
 
-    emulator.screen.test_display();
     spawn_local(async move { emulator.start().await });
 }
